a2f9ce5b5484c515fd712330c361fcc779005be8
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 07:57:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,9 @@ def batch_badge_creator(array)
 end 
 
 def assign_rooms(array)
-  array.each_with_index do |name|
+  room_assignments = []
+  array.each_with_index do |name, index|
+    assignment = "Hello, #{name}! You'll be assigned to room #{index + 1}"
     
   end
 
